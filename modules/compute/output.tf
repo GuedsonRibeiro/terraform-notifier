bba92a9ac294a693c762881bf3bc6a5b81950441
Kledson Basso <kledsonhugo@gmail.com>
2022-09-22 13:22:16 -0300

    Moving Security Group to Network Module

--- modules/compute/output.tf
+++
@@ -1,8 +0,0 @@
-# COMPUTE OUTPUT TO BE REUSED
-output "ec2_sg_pub_id" {
-    value = "${aws_security_group.ec2_sg_pub.id}"
-}
-
-output "ec2_sg_priv_id" {
-    value = "${aws_security_group.ec2_sg_priv.id}"
-}
