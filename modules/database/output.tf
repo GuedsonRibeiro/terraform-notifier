5757f068e716cd002ee93447da201985a546167c
Kledson Basso <kledsonhugo@gmail.com>
2022-09-22 14:49:19 -0300

    Terraform All Modules

--- modules/database/output.tf
+++ modules/database/output.tf
@@ -2,3 +2,11 @@
 output "rds_endpoint" {
     value = "${aws_db_instance.rds_dbinstance.endpoint}"
 }
+
+variable "db_endpoint" {
+    default = "true"
+}
+
+output "aws_db_instance_rds_dbinstance_endpoint" {
+    value = "${var.db_endpoint =="true"? aws_db_instance.rds_dbinstance.endpoint : ""}"
+}
