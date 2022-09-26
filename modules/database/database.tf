c0e19751ff17ff18df7addc22b5eb90068df2654
Kledson Basso <kledsonhugo@gmail.com>
2022-09-26 19:57:57 -0300

    Terraform Modules All

--- modules/database/database.tf
+++ modules/database/database.tf
@@ -42,5 +42,4 @@ resource "aws_db_instance" "rds_dbinstance" {
     parameter_group_name   = aws_db_parameter_group.rds_param_group.name
     availability_zone      = "${var.vpc_az1}"
     vpc_security_group_ids = ["${var.vpc_sg_priv_id}"]
-    multi_az               = true
 }
